720e5d83c2ac4f1a759c579ff296a47915b61a71
D.Hrulev <dmitryhrulev@gmail.com>
2021-03-02 11:29:14 +0300

    Final fixed

--- src/index.js
+++ src/index.js
@@ -3,12 +3,6 @@ import '../src/style.scss'
 let submit = document.querySelector('#submit')
 submit.addEventListener('click', validateEmail)
 
-let subscribe = document.querySelector('.subscribe')
-
-let errorMessage = document.createElement('SPAN')
-errorMessage.innerHTML = 'Please provide a valid adress'
-errorMessage.classList.add('errorMessage')
-
 function validateEmail() {
    let reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/
    let email = document.querySelector('#email')
@@ -16,10 +10,10 @@ function validateEmail() {
    if (reg.test(email.value) === false) {
       email.classList.remove('valid')
       email.classList.add('not_valid')
-      email.insertAdjacentElement('afterend', errorMessage)
+      document.querySelector('#error_label').innerHTML = 'Please provide a valid adress'
    } else {
       email.classList.add('valid')
       email.classList.remove('not_valid')
-      document.querySelector('.errorMessage').remove()
+      document.querySelector('#error_label').innerHTML = ''
    }
 }
